3a996205ee2abf74a936850b5d2df6cf9343e6c5
thespecialone1 <s3neo36@gmail.com>
2024-09-30 22:17:48 +0400

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,7 @@ func main() {
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("Server Closed\n")
 	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
+		fmt.Printf("Error starting the server: %s\n", err)
 		os.Exit(1)
 	}
 }
